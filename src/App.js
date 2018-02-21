52015fe44ff6a57c0e585c5b3c0fd0cf92119aac
Diego Arturo Hernandez Fuentes <diegohdez12@gmail.com>
2018-02-21 11:38:47 -0600

    refactor(App):
    add return with div block

--- src/App.js
+++ src/App.js
@@ -2,7 +2,12 @@ import React from 'react';
 
 class App extends React.Component{
   render() {
-    return React.createElement('h1', null,'Hello World')
+    return (
+      <div>
+        <h1>Hello World</h1> 
+        <b>bold</b>
+      </div>
+    )
   }
 }
 
