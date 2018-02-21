4f745ef8d9e247260bfd09622a9a61058e3f77c1
Diego Arturo Hernandez Fuentes <diegohdez12@gmail.com>
2018-02-21 11:44:50 -0600

    feat(App):
    add interpolation to render a property

--- src/App.js
+++ src/App.js
@@ -2,10 +2,11 @@ import React from 'react';
 
 class App extends React.Component{
   render() {
+    let {txt} = this.props;
     return (
       <div>
-        <h1>Hello World</h1> 
-        <b>bold</b>
+        <h1>{this.props.txt}</h1>
+        <h1>{txt}</h1>
       </div>
     )
   }
