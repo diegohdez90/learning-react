77c7895e4619053bf20b2ede58c4842e111bd539
Diego Arturo Hernandez Fuentes <diegohdez12@gmail.com>
2018-02-22 11:45:26 -0600

    refactor(app):
    add child component to render nested data

--- src/App.js
+++ src/App.js
@@ -3,14 +3,6 @@ import PropTypes from 'prop-types'
 
 class App extends React.Component{
 
-  constructor () {
-    super();
-    this.state ={
-      txt: 'this is the set state',
-      cat: 0,
-    }
-  }
-
   update = event => {
     this.setState({
       txt: event.target.value,
@@ -18,26 +10,20 @@ class App extends React.Component{
   }
 
   render() {
-    let {txt, cat} = this.props;
     return (
       <div>
-        <h1>{this.props.txt}</h1>
-        <h1>{txt}</h1>
-        <p>The cat value is {cat}</p>
-        <div>
-          <label>
-            txt: <input type="text" onChange={this.update} />
-          </label>
-          <p>{this.state.txt} - {this.state.cat}</p>
-        </div>
+        <Button>I <Heart /> React</Button>
       </div>
     )
   }
 }
 
-App.propTypes = {
-  txt: PropTypes.string,
-  cat: PropTypes.number.isRequired
+const Button = props => <button>{props.children}</button>
+
+class Heart extends React.Component {
+  render () {
+    return <span>&hearts;</span>
+  }
 }
 
 export default App;
