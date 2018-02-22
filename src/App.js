d0d9f3bbb5b11f4cf383cca0e47d60dfe72b1e1e
Diego Arturo Hernandez Fuentes <diegohdez12@gmail.com>
2018-02-22 16:57:02 -0600

    feat(app):
    user react.cloneElement to copy button functionality to display button value pressed

--- src/App.js
+++ src/App.js
@@ -1,43 +1,43 @@
 import React from 'react';
-import PropTypes from 'prop-types'
 
 class App extends React.Component{
 
-  constructor () {
-    super();
-    this.state ={
-      txt: 'this is the set state',
-      cat: 0,
-    }
-  }
-
-  update = event => {
-    this.setState({
-      txt: event.target.value,
-    })
-  }
-
   render() {
-    let {txt, cat} = this.props;
     return (
-      <div>
-        <h1>{this.props.txt}</h1>
-        <h1>{txt}</h1>
-        <p>The cat value is {cat}</p>
-        <div>
-          <label>
-            txt: <input type="text" onChange={this.update} />
-          </label>
-          <p>{this.state.txt} - {this.state.cat}</p>
-        </div>
-      </div>
+      <Buttons>
+        <button value="A">A</button>
+        <button value="B">B</button>
+        <button value="C">C</button>
+      </Buttons>
     )
   }
 }
 
-App.propTypes = {
-  txt: PropTypes.string,
-  cat: PropTypes.number.isRequired
+class Buttons extends React.Component {
+  constructor () {
+    super()
+    this.state = {
+      selected: 'None'
+    }
+  }
+
+  selectItem (selected){
+    this.setState({selected})
+  }
+
+  render () {
+    let fn = child => React.cloneElement(child, {
+      onClick: this.selectItem.bind(this, child.props.value)
+    })
+    let items = React.Children.map(this.props.children, fn);
+    return (
+      <div>
+        <h2>You have Selected: {this.state.selected}</h2>
+        {items}
+      </div>
+    )
+  }
+
 }
 
 export default App;
