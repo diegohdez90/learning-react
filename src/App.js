208d4676b898e37b5ecb954b529b60fac6d2d991
Diego Arturo Hernandez Fuentes <diegohdez12@gmail.com>
2018-02-22 10:58:11 -0600

    refactor(App):
    add state component variables to see how state update in onChange input

--- src/App.js
+++ src/App.js
@@ -2,6 +2,21 @@ import React from 'react';
 import PropTypes from 'prop-types'
 
 class App extends React.Component{
+
+  constructor () {
+    super();
+    this.state ={
+      txt: 'this is the set state',
+      cat: 0,
+    }
+  }
+
+  update = event => {
+    this.setState({
+      txt: event.target.value,
+    })
+  }
+
   render() {
     let {txt, cat} = this.props;
     return (
@@ -9,6 +24,12 @@ class App extends React.Component{
         <h1>{this.props.txt}</h1>
         <h1>{txt}</h1>
         <p>The cat value is {cat}</p>
+        <div>
+          <label>
+            txt: <input type="text" onChange={this.update} />
+          </label>
+          <p>{this.state.txt} - {this.state.cat}</p>
+        </div>
       </div>
     )
   }
